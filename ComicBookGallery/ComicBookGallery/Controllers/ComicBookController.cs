6e52acf4f39ba1f50232383659609da321f8237d
Umar Waliyullah <umar.mw@gmail.com>
2016-10-24 14:53:26 +0400

    Add comic book repo

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBookController.cs
@@ -19,18 +19,7 @@ namespace ComicBookGallery.Controllers
 
             var comicBook = new ComicBook()
             {
-                SeriesTitle = "The amazing Spider-Man",
-                IssueNumber = 700,
-                Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Role = "Script",  Name= "Dan Slott"},
-                    new Artist() { Role = "Pencils", Name = "Humberto Ramos"},
-                    new Artist() { Role = "Inks", Name = "Victor Olazaba"},
-                    new Artist() { Role = "Colors", Name = "Edgar Delgado"},
-                    new Artist() { Role = "Letters", Name = "Chris Eliopoulos"}
-
-                }
+                
             };
 
             return View(comicBook);
